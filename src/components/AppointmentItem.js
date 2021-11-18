1134c23c9ada6aa7fb84d2085def6c86b431fa02
Mahziar Talebi <mahziartalebi@gmail.com>
2021-11-18 18:09:36 +0330

    Some bugs fixed

--- src/components/AppointmentItem.js
+++
@@ -1,8 +0,0 @@
-export default function AppointmentItem(props) {
-    return (
-        <div>
-            <span className="app-info-title">{props.title}</span>
-            <span className="app-info-description">{props.description}</span>
-        </div>
-    );
-};
