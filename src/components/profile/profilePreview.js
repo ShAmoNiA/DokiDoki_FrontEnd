3f803d34380ea3f554f441ce91e565f16efe9c09
DabirRahmani <trtpta@gmail.com>
2021-11-23 02:20:18 +0330

    profile preview changed to modal components

--- src/components/profile/profilePreview.js
+++ src/components/profile/profilePreview.js
@@ -4,7 +4,9 @@ import React, { useEffect, useState } from "react";
 import { LoadingGif } from "../../asset/svgIcons";
 import { BackendImageAdress } from "../../backend/address";
 import GetProfileDetailsRequest from "../../backend/User/Profile/getProfileDetails";
+import GetProfilePreviewDetailsRequest from "../../backend/User/Profile/getProfilePreviewDetails";
 import MainAvatar from "../avatar/avatar";
+import { Modal } from "antd";
 
 const fontColorItem = "gray";
 const fontColorValue = "black";
@@ -44,29 +46,44 @@ const ItemWithValue = {
   borderWidth: 1,
 };
 
-const ProfilePreview = ({ editpofile, reload }) => {
-  const [profileDetails, setProfileDetails] = useState("");
+const ProfilePreview = ({ username }) => {
+  const [mUsername, setMUsername] = useState("");
+  const [profileDetails, setProfileDetails] = useState({});
+
+  const [modalStatus, setModalStatus] = useState(false);
 
   useEffect(() => {
-    GetProfileDetailsRequest({
-      datacaller: (data) => {
-        if (data.error) {
-          console.log("error in getting profile details");
-        } else {
-          console.log(data.expertise_tags);
-          setProfileDetails({
-            ...data,
-            expertise_tags:
-              "maghz_va_asab roode dastgahe_govaresh dastgahe_gvaresh dastgahe_garesh dastgahe_gesh",
-          });
-        }
-      },
-    });
+    if (mUsername !== "")
+      GetProfilePreviewDetailsRequest({
+        username: mUsername,
+        datacaller: (data) => {
+          console.log(data);
+          if (data.error) {
+            console.log("error in getting profile details");
+          } else {
+            console.log(data.expertise_tags);
+            setProfileDetails({
+              ...data,
+              expertise_tags:
+                "maghz_va_asab roode dastgahe_govaresh dastgahe_gvaresh dastgahe_garesh dastgahe_gesh",
+            });
+          }
+        },
+      });
     //اول اطلاعات پروفايل رو دريافت ميکنيم تو اين کامپوننت
     // دقت کن که اين تگ ها بايد درست شن
     //الان روي حالت تست تگ ها هستيم
+  }, [mUsername]);
+
+  useEffect(() => {
     setProfileDetails({});
-  }, [reload]);
+    if (username !== undefined) {
+      if (username !== "") {
+        setMUsername(username);
+        setModalStatus(true);
+      } else setMUsername("");
+    } else setMUsername("");
+  }, [username]);
 
   const CreateExpertisesTags = () => {
     var tags = profileDetails.expertise_tags + "";
@@ -140,9 +157,9 @@ const ProfilePreview = ({ editpofile, reload }) => {
           <div
             style={{
               flex: 1,
-              minWidth: 350,
-              marginLeft: 32,
-              marginRight: 32,
+              minWidth: 250,
+              marginLeft: 8,
+              marginRight: 8,
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-around",
@@ -157,13 +174,13 @@ const ProfilePreview = ({ editpofile, reload }) => {
               </div>
             </div>
             <div style={{ ...ItemWithValue }}>
-              <div>Curriculum vitae:</div>
+              <div>C.V:</div>
               <div style={{ ...ItemValueText }}>
                 {profileDetails.cv === null ? "N/A" : profileDetails.cv}
               </div>
             </div>
             <div style={{ ...ItemWithValue }}>
-              <div>Office location:</div>
+              <div>Adress:</div>
               <div style={{ ...ItemValueText }}>
                 {profileDetails.office_location === null
                   ? "N/A"
@@ -206,9 +223,9 @@ const ProfilePreview = ({ editpofile, reload }) => {
           <div
             style={{
               flex: 1,
-              minWidth: 350,
-              marginLeft: 32,
-              marginRight: 32,
+              minWidth: 250,
+              marginLeft: 8,
+              marginRight: 8,
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-around",
@@ -245,7 +262,6 @@ const ProfilePreview = ({ editpofile, reload }) => {
             flexWrap: "wrap",
             alignItems: "stretch",
             backgroundColor: "white",
-            ...BottomBorder,
           }}
         >
           <div
@@ -264,9 +280,9 @@ const ProfilePreview = ({ editpofile, reload }) => {
           <div
             style={{
               flex: 1,
-              minWidth: 350,
-              marginLeft: 32,
-              marginRight: 32,
+              minWidth: 250,
+              marginLeft: 8,
+              marginRight: 8,
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-around",
@@ -290,51 +306,15 @@ const ProfilePreview = ({ editpofile, reload }) => {
                   : profileDetails.weight + " kg"}
               </div>
             </div>
-          </div>
-        </div>
 
-        <div
-          style={{
-            padding: 16,
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-            alignItems: "stretch",
-            backgroundColor: "white",
-          }}
-        >
-          <div
-            style={{
-              alignItems: "center",
-              justifyContent: "center",
-              display: "flex",
-              width: 150,
-              ...ItemValueText,
-              fontSize: 16,
-            }}
-          >
-            Medical records
-          </div>
-
-          <div
-            style={{
-              flex: 1,
-              marginLeft: 32,
-              marginRight: 32,
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "flex-end",
-              flexWrap: "nowrap",
-              ...ItemText,
-              ...ItemValueText,
-              maxWidth: 350,
-              textAlign: "center",
-            }}
-          >
-            {profileDetails.medical_records === null
-              ? "No Records"
-              : profileDetails.medical_records}
+            <div style={{ ...ItemWithValue }}>
+              <div>Medical Records:</div>
+              <div style={{ ...ItemValueText }}>
+                {profileDetails.medical_records === null
+                  ? "No Records"
+                  : profileDetails.medical_records}
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -350,110 +330,113 @@ const ProfilePreview = ({ editpofile, reload }) => {
     }
   };
 
-  if (profileDetails.username === undefined) {
-    return (
-      <div>
-        <LoadingGif height={100} />
-      </div>
-    );
-  } else
-    return (
-      <div>
-        <div
-          style={{
-            padding: 16,
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            flexWrap: "wrap",
-            alignItems: "stretch",
-            backgroundColor: "white",
-            ...BottomBorder,
-          }}
-        >
+  const CreateContent = () => {
+    if (profileDetails.username === undefined) {
+      return (
+        <div>
+          <LoadingGif height={100} />
+        </div>
+      );
+    } else
+      return (
+        <div>
           <div
             style={{
-              alignItems: "center",
+              padding: 16,
+              display: "flex",
+              flexDirection: "row",
               justifyContent: "center",
-              display: "flex",
-              marginLeft: 16,
-              flexDirection: "column",
-            }}
-          >
-            <Avatar
-              src={BackendImageAdress + profileDetails.profile_picture_url}
-              style={{ width: 150, height: 150 }}
-            />
-            <div style={{ marginTop: 15 }}>
-              <span className="info-block-button" onClick={editpofile}>
-                EDIT PROFILE
-              </span>
-            </div>
-          </div>
-          <div
-            style={{
-              flex: 1,
-              minWidth: 350,
-              marginLeft: 32,
-              marginRight: 32,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-around",
-              flexWrap: "nowrap",
-              ...ItemText,
+              flexWrap: "wrap",
+              alignItems: "stretch",
+              backgroundColor: "white",
+              ...BottomBorder,
             }}
           >
             <div
               style={{
-                ...NameText,
-                ...ItemValueText,
-                alignSelf: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                display: "flex",
+                marginLeft: 16,
+                flexDirection: "column",
               }}
             >
-              <div>
-                {profileDetails.fullname === null
-                  ? "No Full Name"
-                  : profileDetails.fullname}
-              </div>
+              <Avatar
+                src={BackendImageAdress + profileDetails.profile_picture_url}
+                style={{ width: 150, height: 150 }}
+              />
             </div>
-            <div style={{ ...ItemWithValue }}>
-              <div>Username:</div>
-              <div style={{ ...ItemValueText }}>
-                {profileDetails.username === null
-                  ? "N/A"
-                  : profileDetails.username}
+            <div
+              style={{
+                flex: 1,
+                minWidth: 250,
+                marginLeft: 8,
+                marginRight: 8,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around",
+                flexWrap: "nowrap",
+                ...ItemText,
+              }}
+            >
+              <div
+                style={{
+                  ...NameText,
+                  ...ItemValueText,
+                  alignSelf: "center",
+                }}
+              >
+                <div>
+                  {profileDetails.fullname === null
+                    ? "No Full Name"
+                    : profileDetails.fullname}
+                </div>
               </div>
-            </div>
-            <div style={{ ...ItemWithValue }}>
-              <div>Phone number:</div>
-              <div style={{ ...ItemValueText }}>
-                {profileDetails.phone === null ? "N/A" : profileDetails.phone}
+              <div style={{ ...ItemWithValue }}>
+                <div>Username:</div>
+                <div style={{ ...ItemValueText }}>
+                  {profileDetails.username === null
+                    ? "N/A"
+                    : profileDetails.username}
+                </div>
               </div>
-            </div>
-            <div style={{ ...ItemWithValue }}>
-              <div>Email:</div>
-              <div style={{ ...ItemValueText }}>
-                {profileDetails.email === null ? "N/A" : profileDetails.email}
-              </div>
-            </div>
 
-            <div style={{ ...ItemWithValue }}>
-              <div>Sex:</div>
-              <div style={{ ...ItemValueText }}>
-                {profileDetails.sex === "M" ? "Male" : ""}
-                {profileDetails.sex === "F" ? "Female" : ""}
+              <div style={{ ...ItemWithValue }}>
+                <div>Sex:</div>
+                <div style={{ ...ItemValueText }}>
+                  {profileDetails.sex === "M" ? "Male" : ""}
+                  {profileDetails.sex === "F" ? "Female" : ""}
 
-                {profileDetails.sex === "U" ? "Unsure" : ""}
+                  {profileDetails.sex === "U" ? "Unsure" : ""}
 
-                {profileDetails.sex === "P" ? "Prefer not to say" : ""}
+                  {profileDetails.sex === "P" ? "Prefer not to say" : ""}
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div style={{ background: "red" }}>{CreateProfileDetails()}</div>
+          <div style={{ background: "red" }}>{CreateProfileDetails()}</div>
+        </div>
+      );
+  };
+
+  return (
+    <Modal
+      footer={null}
+      onCancel={() => {
+        setMUsername("");
+        setModalStatus(false);
+      }}
+      width={750}
+      visible={modalStatus}
+      maskClosable={false}
+      centered
+    >
+      <div style={{ height: 400, overflow: "auto", marginTop: 25 }}>
+        {CreateContent()}
       </div>
-    );
+    </Modal>
+  );
 };
 
 export default ProfilePreview;
