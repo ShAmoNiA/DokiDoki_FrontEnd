c02852c06aabe84e9b71f0b753d54c8209e12630
shayan <shamousavinia@gmail.com>
2021-12-26 11:08:46 +0330

    add comment

---
+++ src/components/comment/comment.js
@@ -0,0 +1,106 @@
+import './App.css';
+import React from 'react';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { Input,Button } from 'antd';
+import { Comment, Tooltip, Avatar, Card } from 'antd';
+import 'antd/dist/antd.css'
+import moment from 'moment';
+import './comment.css';
+
+export default function App({doctor_Id}) {
+
+  const [dataFromIn,setIn] = useState(0)
+  const [data,setData] = useState([]);
+  const doctorId = 2
+
+  function CommentStrunt ({ authorName,contentText ,data})  {
+    return (
+      <Card>
+        <Comment
+          author={<a>{authorName}</a>}
+          content={
+            <p>
+              {contentText}
+            </p>
+          }
+          datetime={
+            <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+              <span>{data}</span>
+            </Tooltip>
+          }
+        />
+      </Card>
+      );
+  };
+  
+  function CommentSub ({doctorId}) {
+
+    const [text,setText] = useState("")
+    const { TextArea } = Input;
+  
+    const onChange = e => {
+        setText(e.target.value)
+        console.log('Change:', e.target.value);
+    };
+  
+  
+    function clickHandler() {   
+        var formData = new FormData();
+        formData.append("doctor_id", doctorId);
+        formData.append("text", text);
+        axios({
+            method: "post",
+            url: "http://185.141.107.81:1111/api/new_comment",
+            data: formData,
+            headers: {
+                Authorization: "token"
+              }
+          })
+            .then(function (response) {
+              if (response.status == 200)
+                setText("")
+                if (dataFromIn == 0) {
+                  setIn(1)
+                } else {
+                  setIn(0)
+                }
+              console.log(response);
+            })
+            .catch(function (response) {
+              console.log(response);
+            });
+        }
+  
+    return(
+        <div>
+        <TextArea showCount value={text} maxLength={100} style={{ height: 120 }} onChange={onChange} />
+        <Button onClick={clickHandler} htmlType="submit" type="primary">
+            Add Comment
+        </Button>
+        </div>
+        
+    )
+  };  
+  useEffect(() => {
+    axios.get("http://185.141.107.81:1111/api/comments/"+doctorId)
+    .then((response) => {
+      setData(response.data.comments)
+      console.log(response)
+    })
+    .catch(() => {
+      console.log("error in getting resault from server");
+      setData([]);
+    })
+  },[dataFromIn]);
+
+  return (
+    <div>
+      {data.map((s)=>(
+        <CommentStrunt authorName ={s.writer_name} contentText={s.text} data={s.date}/>
+      ))}
+      <CommentSub doctorId={doctor_Id}/>
+    </div>
+  );
+}
+
